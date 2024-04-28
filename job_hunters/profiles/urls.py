d9bf9c7d7664c9421320c62c8aa39f6f7fa26681
Aron Elí Herreros <aroneh19@gmail.com>
2024-04-28 19:11:12 +0000

    changed settins.py

---
+++ job_hunters/profiles/urls.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.views import (
+    LoginView, LogoutView, PasswordResetView,
+    PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView)
+from django.urls import path
+from . import views
+
+urlpatterns = []
