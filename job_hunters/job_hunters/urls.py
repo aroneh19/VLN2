2d6a76174fccca78c4ddf323df82fec5222d2fb7
Aron Elí Herreros <aroneh19@gmail.com>
2024-04-29 11:23:57 +0000

    fixed urls

--- job_hunters/job_hunters/urls.py
+++ job_hunters/job_hunters/urls.py
@@ -20,6 +20,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('profiles/', include('profiles.urls'))
+    # path('admin/', admin.site.urls),
+    # path('profiles/', include('profiles.urls'))
 ]
