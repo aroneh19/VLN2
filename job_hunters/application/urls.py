0490c659984c0fcb1f725308ae00d9b13e43be00
tommibent8 <144326353+tommibent8@users.noreply.github.com>
2024-05-06 13:30:30 +0000

    user_applications

--- job_hunters/application/urls.py
+++ job_hunters/application/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('application.html/', views.application_form, name="Application_Form")
+    path('application.html/', views.application_form, name="Application_Form"),
+    path('user_applications.html/', views.user_application, name="User_Applications")
 ]
--- job_hunters/application/views.py
+++ job_hunters/application/views.py
@@ -4,3 +4,6 @@ from .models import Application
 # Create your views here.
 def application_form(request):
     return render(request, 'application/application.html')
+
+def user_application(request):
+    return render(request, 'application/user_application.html')
