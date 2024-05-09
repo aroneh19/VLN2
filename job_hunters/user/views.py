61e2a9b8e68d2774a93d29944af407e0c24e0bea
Solvi2002 <151065525+Solvi2002@users.noreply.github.com>
2024-05-09 18:06:50 +0000

    some fixes

--- job_hunters/user/views.py
+++ job_hunters/user/views.py
@@ -1,20 +1,28 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.hashers import make_password
-from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
-from .models import User, Country, Location
+from django.contrib.auth.models import User
+from .models import Profile, Country, Location
 from .forms import ProfileRegistrationForm
 
 
 def edit_user(request):
-    user_id = 3
-    user = User.objects.get(uid=user_id)
-    return render(request, 'user/edit.html', {'user': user})
+    countries = Country.objects.all()
+    locations = Location.objects.all()
+    user_id = 2
+    p = Profile()
+    user = Profile.objects.get(user=user_id)
+    context = {
+        'user': user,
+        'countries': countries,
+        'locations': locations
+    }
+    return render(request, 'user/edit.html', context)
 
 def profile_user(request):
-    user_id = 3
-    user = User.objects.get(uid=user_id)
+    user_id = 2
+    user = Profile.objects.get(user_id=user_id)
     return render(request, 'user/profile.html', {'user': user})
 
 def register_user(request):
