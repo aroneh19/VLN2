2907aaac86947250b036f1a1cb43b6a4c708887e
Aron Elí Herreros <aroneh19@gmail.com>
2024-05-09 12:43:04 +0000

    Update forms.py

--- job_hunters/user/forms.py
+++ job_hunters/user/forms.py
@@ -3,6 +3,13 @@ from django import forms
 from .models import Profile
 
 class ProfileRegistrationForm(forms.ModelForm):
+    username = forms.CharField(label='Social Security Number', max_length=100)
+    first_name = forms.CharField(label='First Name', max_length=100)
+    last_name = forms.CharField(label='Last Name', max_length=100)
+    email = forms.EmailField(label='Email')
+    password = forms.CharField(widget=forms.PasswordInput)
     class Meta:
         model = Profile
         fields = ['phone', 'street_name', 'house_number', 'date_of_birth', 'picture', 'location', 'country']
+
+
