55f1c9015f95762f0735d038eecf0de2556fab4f
Aron Elí Herreros <aroneh19@gmail.com>
2024-05-09 18:05:02 +0000

    changed model

--- job_hunters/company/models.py
+++ job_hunters/company/models.py
@@ -4,10 +4,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Company(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    name = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
-    logo = models.URLField(default="http://t1.gstatic.com/licensed-image?q=tbn:ANd9GcR0NrOJEpfjkM0zxD-aO9b-bWqW3mhY57jPMg3aSbxTYO__R4jOvx8T2Oa7Fm9yxXOGg4B_ns3SZaZGCiBOPQw")
-    cover = models.URLField(default="http://t1.gstatic.com/licensed-image?q=tbn:ANd9GcR0NrOJEpfjkM0zxD-aO9b-bWqW3mhY57jPMg3aSbxTYO__R4jOvx8T2Oa7Fm9yxXOGg4B_ns3SZaZGCiBOPQw")
+    logo = models.URLField()
+    cover = models.URLField()
     description = models.TextField()
     website = models.URLField()
 
