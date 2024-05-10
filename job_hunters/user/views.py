890889e97c86d1719868df247b765940937ef801
Aron Elí Herreros <aroneh19@gmail.com>
2024-05-10 00:27:46 +0000

    added decorators for users

--- job_hunters/user/views.py
+++ job_hunters/user/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 from .models import Profile, Country, Location
 from .forms import CustomUserCreationForm
 
@@ -40,3 +41,12 @@ def register_user(request):
     
     
     return render(request, 'user/register.html', {'form': form, 'countries': countries, 'locations': locations})
+
+
+@login_required
+def profile_view(request):
+    return render(request, "user/profile.html")
+
+@login_required
+def edit_profile(request):
+    return render(request, "user/edit.html")
