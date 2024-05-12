d64523c4558e34a134776affbec8064ab4d46c68
TomasKR15 <144327870+TomasKR15@users.noreply.github.com>
2024-05-12 16:32:44 +0000

    navbar css

--- job_hunters/job_hunters/views.py
+++ job_hunters/job_hunters/views.py
@@ -20,17 +20,7 @@ def home(request):
     context = {
         'jobs': jobs,
     }
-    if request.user.is_authenticated:
-        is_company = Company.objects.filter(user=request.user).exists()
-        if is_company:
-            photo = Company.objects.get(user=request.user).logo
-        else:
-            photo = Profile.objects.get(user=request.user).picture
-        context.update({
-            'is_company': is_company,
-            'photo': photo,
-        })
-    return render(request, 'index.html', context)
+    return render(request, 'index.html')
 
 
 def handler404(request, *args, **argv):
