2af8cc183c0eb7f8d2d0839b98ff831460d22d8f
Aron Elí Herreros <aroneh19@gmail.com>
2024-05-12 12:02:53 +0000

    added profile image in the navbar

--- job_hunters/job_hunters/views.py
+++ job_hunters/job_hunters/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from company.models import Company 
+from user.models import Profile
+from company.models import Company
 from django.contrib.auth.models import User
 
 
@@ -12,8 +13,13 @@ def jobs():
 def home(request):
     if request.user.is_authenticated:
         is_company = Company.objects.filter(user=request.user).exists()
+        if is_company:
+            photo = Company.objects.get(user=request.user).logo
+        else:
+            photo = Profile.objects.get(username=request.user).picture
         context = {
-            'is_company': is_company
+            'is_company': is_company,
+            'photo': photo,
         }
         return render(request, 'index.html', context)
     return render(request, 'index.html')
