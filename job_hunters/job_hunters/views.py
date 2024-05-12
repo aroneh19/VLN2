c9b99362789587b9b3c3037cf059b8d89d160515
Aron Elí Herreros <aroneh19@gmail.com>
2024-05-12 14:00:28 +0000

    trying to do random jobs

--- job_hunters/job_hunters/views.py
+++ job_hunters/job_hunters/views.py
@@ -2,27 +2,34 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from user.models import Profile
 from company.models import Company
-from django.contrib.auth.models import User
+from job.models import Job
+from random import sample, seed
+from datetime import date
 
 
-def jobs():
-    # Sækja störf sem henta á home page
-    pass
+def random_daily_jobs(num_jobs = 8):
+    jobs = list(Job.objects.filter(start_date__lte=date.today(), end_date__gte=date.today()))
+    seed(str(date.today))
+    sampled_jobs = sample(jobs, num_jobs)
+    return sampled_jobs
 
 
 def home(request):
+    jobs = random_daily_jobs()
+    context = {
+        'jobs': jobs,
+    }
     if request.user.is_authenticated:
         is_company = Company.objects.filter(user=request.user).exists()
         if is_company:
             photo = Company.objects.get(user=request.user).logo
         else:
             photo = Profile.objects.get(user=request.user).picture
-        context = {
+        context.update({
             'is_company': is_company,
             'photo': photo,
-        }
-        return render(request, 'index.html', context)
-    return render(request, 'index.html')
+        })
+    return render(request, 'index.html', context)
 
 
 def handler404(request, *args, **argv):
