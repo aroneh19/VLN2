97fdaec19f4e4a649057a96dbed6448f94ad6821
tommibent8 <144326353+tommibent8@users.noreply.github.com>
2024-05-12 17:39:03 +0000

    steps in application... and filtering

--- job_hunters/job/urls.py
+++ job_hunters/job/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('jobs.html/', views.job, name='jobs'),
-    path('job/jobs.html/', views.filter_job_offerings, name='filter_job_offerings')
+    path('job/jobs.html/', views.filter_job_offerings, name='filter_job_offerings'),
+    path('job_info.html/',views.job_info, name="job_info" )
 ]
