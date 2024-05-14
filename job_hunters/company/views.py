5acbd8a48b3a79805c6ba4b71cbb7a28838d86ba
Aron Elí Herreros <aroneh19@gmail.com>
2024-05-14 11:50:15 +0000

    doing company/ies view

--- job_hunters/company/views.py
+++ job_hunters/company/views.py
@@ -41,6 +41,18 @@ def login_view(request):
     return render(request, 'company/login.html', context)
 
 
+
+def companies_view(request):
+    companies = Company.objects.all()
+    context = {
+        'companies': companies
+    }
+    return render(request, 'company/companies.html', context)
+
+
+def company_view(request):
+    return render(request, 'company/company.html')
+
 @login_required
 def profile_view(request):
     context = {
