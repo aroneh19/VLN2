887c37b8c7d874125919fb5c381ccb9219d066df
Aron Elí Herreros <aroneh19@gmail.com>
2024-05-16 18:08:56 +0000

    slight fixes to index

--- job_hunters/job_hunters/views.py
+++ job_hunters/job_hunters/views.py
@@ -7,7 +7,7 @@ from random import sample, seed
 from datetime import date
 
 
-def random_daily_jobs(num_jobs = 4):
+def random_daily_jobs(num_jobs = 7):
     date_today = date.today()
     jobs = list(Job.objects.filter(start_date__lte=date_today, due_date__gte=date_today))
     seed(str(date.today))
