7f9c6b35ac5dc8efbbd59da91fca83d5484cd6cc
Aron Elí Herreros <aroneh19@gmail.com>
2024-05-16 20:40:48 +0000

    changed job_hunters views.py

--- job_hunters/job_hunters/views.py
+++ job_hunters/job_hunters/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from user.models import Profile
-from company.models import Company
 from job.models import Job
 from random import sample, seed
 from datetime import date
@@ -18,7 +16,6 @@ def random_daily_jobs(num_jobs = 8):
     sampled_jobs = sample(jobs, num_jobs)
     return sampled_jobs
 
-
 def home(request):
     jobs = random_daily_jobs()
     context = {
@@ -28,7 +25,6 @@ def home(request):
 
 def zavant(request):
     return render(request, 'why_zavant.html')
-
 
 def handler404(request, *args, **argv):
     messages.error(request, 'The page you were looking does not exist.')
