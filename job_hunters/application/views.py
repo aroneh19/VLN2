41d9aa3b59ca44bf471456373e9f60f50323b122
Aron Elí Herreros <aroneh19@gmail.com>
2024-05-17 18:51:29 +0000

    doing gods work

--- job_hunters/application/views.py
+++ job_hunters/application/views.py
@@ -1,25 +1,59 @@
 from django.shortcuts import render, redirect
 from .models import Application
 from .forms import ApplicationForm
-from user.models import Recommendation, Experience, Country, Profile
+from user.models import Recommendation, Experience, Profile
 from django.contrib.auth.decorators import login_required
 
 @login_required
 def application_form(request):
     job = request.GET.get('job_jid')
-    user_profile = Profile.objects.get(user=request.user.id)  # Retrieve the Profile object for the logged-in user
+    user_profile = Profile.objects.get(user=request.user.id)
     recommendations = Recommendation.objects.filter(profile=user_profile)
     experiences = Experience.objects.filter(profile=user_profile)
-    countries = Country.objects.all()
+
+    if request.method == 'POST':
+        form = ApplicationForm(request.POST)
+    else:
+        form = ApplicationForm()
 
     context = {
         'recommendations': recommendations,
         'experiences': experiences,
-        'countries': countries,
         'user_profile': user_profile,
-        'job': job
+        'job': job,
+        'form': form
     }
     return render(request, 'application/application.html', context)
+
+
+@login_required
+def review_application(request):
+    user_profile = Profile.objects.get(user=request.user.id)
+    recommendations = Recommendation.objects.filter(profile=user_profile)
+    experiences = Experience.objects.filter(profile=user_profile)
+
+    if request.method == 'POST':
+        form = ApplicationForm(request.POST)
+        if form.is_valid():
+            application = form.save(commit=False)
+            application.user = request.user
+            application.save()
+            return redirect('confirmation_page')
+        else:
+            return
+    else:
+        application = Application.objects.filter(user=request.user).first()
+        cover_letter = application.cover_letter if application else None
+
+        context = {
+            'user_profile': user_profile,
+            'experiences': experiences,
+            'recommendations': recommendations,
+            'cover_letter': cover_letter,
+            'form': ApplicationForm()
+        }
+        return render(request, 'review_application.html', context)
+
 
 def submit_application(request):
     return render(request, 'application/confirmation.html')
