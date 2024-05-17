a46b3f9224e4380c576750eb255ccfab6429099b
tommibent8 <144326353+tommibent8@users.noreply.github.com>
2024-05-17 22:38:02 +0000

    tomas trying to fix

--- job_hunters/application/views.py
+++ job_hunters/application/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Application
+from job.models import Job
 from .forms import ApplicationForm
 from user.models import Recommendation, Experience, Profile
 from django.contrib.auth.decorators import login_required
@@ -26,6 +27,20 @@ def review(request):
     recommendations = Recommendation.objects.filter(profile=user_profile)
     experiences = Experience.objects.filter(profile=user_profile)
 
+    cover_letter = request.GET.get('cover_letter')
+    job_id = request.GET.get('job_jid')
+    job = Job.objects.get(jid = job_id)
+    context = {
+        'user_profile': user_profile,
+        'experiences': experiences,
+        'recommendations': recommendations,
+        'cover_letter': cover_letter,
+        'job': job,
+        'form': ApplicationForm()
+    }
+    return render(request, 'application/review.html', context)
+
+def submit_review(request):
     if request.method == 'POST':
         form = ApplicationForm(request.POST)
         if form.is_valid():
@@ -34,17 +49,7 @@ def review(request):
             application.save()
             return redirect('application/confirmation_page')
     else:
-        cover_letter = request.POST.get('cover_letter_hidden')
-        application = Application.objects.filter(user=request.user).first()
-
-        context = {
-            'user_profile': user_profile,
-            'experiences': experiences,
-            'recommendations': recommendations,
-            'cover_letter': cover_letter,
-            'form': ApplicationForm()
-        }
-        return render(request, 'application/review.html', context)
+        redirect('jobs')
 
 @login_required
 def confirmation_page(request):
