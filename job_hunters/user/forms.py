907d195186fbbe6d9d496d4a5010b30b19ec4afb
TomasKR15 <144327870+TomasKR15@users.noreply.github.com>
2024-05-17 10:55:31 +0000

    placeholders í register

--- job_hunters/user/forms.py
+++ job_hunters/user/forms.py
@@ -3,6 +3,51 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, User
 from django.contrib.auth.models import User
 from .models import Profile, Recommendation, Experience
 
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(attrs={
+            'placeholder': 'Type in email',
+            'class': 'form-control'
+        })
+    )
+    first_name = forms.CharField(
+        max_length=50,
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Type in first name',
+            'class': 'form-control'
+        })
+    )
+    last_name = forms.CharField(
+        max_length=50,
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Type in last name',
+            'class': 'form-control'
+        })
+    )
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Type in username',
+            'class': 'form-control'
+        })
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Type in password',
+            'class': 'form-control'
+        })
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Confirm password',
+            'class': 'form-control'
+        })
+    )
+
+    class Meta:
+        model = User
+        fields = ("username", "first_name", "last_name", "email", "password1", "password2")
+
 class CustomAuthenticationForm(AuthenticationForm):
     username = forms.CharField(
         widget=forms.TextInput(attrs={'placeholder': 'Type in username', 'class': 'form-control'})
@@ -11,18 +56,18 @@ class CustomAuthenticationForm(AuthenticationForm):
         widget=forms.PasswordInput(attrs={'placeholder': 'Type in password', 'class': 'form-control'})
     )
 
-class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-    first_name = forms.CharField(max_length=50)
-    last_name = forms.CharField(max_length=50)
+# class CustomUserCreationForm(UserCreationForm):
+#     email = forms.EmailField(required=True)
+#     first_name = forms.CharField(max_length=50)
+#     last_name = forms.CharField(max_length=50)
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['username'].help_text = ''
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.fields['username'].help_text = ''
 
-    class Meta:
-        model = User
-        fields = ("username", "first_name", "last_name", "email", "password1", "password2")
+#     class Meta:
+#         model = User
+#         fields = ("username", "first_name", "last_name", "email", "password1", "password2")
 
 class UserChangeForm(BaseUserChangeForm):
     class Meta:
