262348c0766720201928b11a08e559fad1f5cd8b
Aron Elí Herreros <aroneh19@gmail.com>
2024-05-17 13:07:21 +0000

    added decorators

--- job_hunters/application/views.py
+++ job_hunters/application/views.py
@@ -2,15 +2,25 @@ from django.shortcuts import render, redirect
 from .models import Application
 from .forms import ApplicationForm
 from user.models import Recommendation, Experience, Country, Profile
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def application_form(request):
     job = request.GET.get('job_jid')
     user_profile = Profile.objects.get(user=request.user.id)  # Retrieve the Profile object for the logged-in user
     recommendations = Recommendation.objects.filter(profile=user_profile)
     experiences = Experience.objects.filter(profile=user_profile)
     countries = Country.objects.all()
-    return render(request, 'application/application.html', {'recommendations': recommendations, 'experiences': experiences, 'countries': countries, 'user_profile': user_profile, 'job': job})
+    context = {
+        'recommendations': recommendations,
+        'experiences': experiences,
+        'countries': countries,
+        'user_profile': user_profile,
+        'job': job
+    }
+    return render(request, 'application/application.html', context)
 
+@login_required
 def submit_application(request):
     print(request.POST)
     if request.method == 'POST':
@@ -23,8 +33,8 @@ def submit_application(request):
         return redirect('jobs')
     return redirect('jobs')
 
+@login_required
 def user_application(request):
     user_profile = Profile.objects.get(user = request.user.id)
     applications = Application.objects.filter(user=user_profile.user)
     return render(request, 'application/user_applications.html', {'applications': applications})
-
